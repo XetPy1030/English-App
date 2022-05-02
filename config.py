2e22ee3d96e77b172d8d6f7cfde3dca41686b274
XetPy1030 <xetpy1030@mail.ru>
2022-05-02 23:04:28 +0500

    change

---
+++ config.py
@@ -0,0 +1,10 @@
+app_name = "Dictionary"
+app_size = "400x300"
+
+button_search = "search"
+
+not_found = "not found"
+
+info = """Word: {word}
+Translate: {translate}
+Other: {other}"""
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import Listbox, messagebox
 
 import pandas as pd
 from PIL import Image, ImageTk
-import os
+import config
 
 def convert(filename: str) -> list:
     excel = pd.read_excel(filename).to_dict()
@@ -21,7 +21,12 @@ def convert(filename: str) -> list:
 words = convert("words.xlsx")
 
 def click_word(event: tk.Event):
-    pass
+    newWindow = tk.Toplevel(window)
+    labelExample = tk.Label(newWindow, text = "New Window")
+    buttonExample = tk.Button(newWindow, text = "New Window button")
+
+    labelExample.pack()
+    buttonExample.pack()
 
 def set_words(words: list, window: tk.Tk, start_row: int = 2):
     for i in range(len(words)):
@@ -46,17 +51,51 @@ def test_button(event: tk.Event):
     messagebox.showinfo("Перевод", event.widget.cget("text"))
     print(event.widget.cget("text"))
 
+def select_word(event: tk.Event):
+    word = my_list.get(tk.ANCHOR)
+    info = get_info_from_word(word, words)
+
+    newWindow = tk.Toplevel(window)
+
+    labelExample = tk.Label(newWindow, text=config.info.format(word=info["Word"], translate=info["Translate"], other=info["Other"].replace("nan", config.not_found)))
+    buttonExample = tk.Button(newWindow, text = "New Window button")
+
+    labelExample.pack()
+    buttonExample.pack()
+
+def get_info_from_word(word: str, words: list):
+    for i in words:
+        if i["Word"] == word:
+            return i
+    return None
+
 window = tk.Tk()
-window.geometry("400x500")
-window.title("Словарь")
+window.geometry(config.app_size)
+window.title(config.app_name)
+
+def testt():
+    print("wd")
+window.protocol("WM_DELETE_WINDOW", testt)
 
 search_text = tk.StringVar()
 
 #text = tk.Label(window, text="Поиск").grid(row=0)
 input_text = tk.Entry(window, textvariable=search_text).grid(row=0, column=0)
-button = tk.Button(window, text="Поиск", command=search).grid(row=0, column=1)
+button = tk.Button(window, text=config.button_search, command=search).grid(row=0, column=1)
 
-set_words(words, window)
+#set_words(words, window)
+
+my_list = Listbox(window, width=40)
+my_list.grid(row=1, column=0)
+my_list.bind('<<ListboxSelect>>', select_word)
+
+def set_words_list(words: list, window: tk.Tk):
+    global my_list
+
+    for i in range(len(words)):
+        my_list.insert(i+1, words[i]["Word"])
+
+set_words_list(words, window)
 
 """t = tk.Label(window, text="test")
 t.grid(row=1, column=0)
