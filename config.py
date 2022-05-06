4bc0a69920afce7fc7451d5995a1c4a20709d57d
XetPy1030 <xetpy1030@mail.ru>
2022-05-06 14:26:25 +0500

    config

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ app_name = config["DEFAULT"]["app_name"]
 app_size = config["DEFAULT"]["app_size"]
 button_search = config["DEFAULT"]["button_search"]
 not_found = config["DEFAULT"]["not_found"]
-search_in = config["DEFAULT"]["search_in"].split(",")
+search_in = config["DEFAULT"]["search_in"].split(" ")
 info = config["DEFAULT"]["info"]
